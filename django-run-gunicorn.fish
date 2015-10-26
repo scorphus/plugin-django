18285d19a8a7f4871dc790b3de43773d81c99d5a
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-10-26 11:41:23 -0200

    fix(run-gunicorn): use `--` arg to demarcate options to contains

--- django-run-gunicorn.fish
+++ django-run-gunicorn.fish
@@ -10,7 +10,7 @@ function django-run-gunicorn -a p -a w -a l -a t \
   set argn (count $argv)
   test $argn -ge 1; and test $p -ge 1024; or set p 8000           # port
   test $argn -ge 2; and test $w -ge 1; or set w 2                 # workers
-  test $argn -ge 3; and contains "$l" $log_levels; or set l info  # level
+  test $argn -ge 3; and contains -- "$l" $log_levels; or set l info  # level
   test $argn -ge 4; and test $t -ge 60; or set t 600              # timeout
   django-kill-test-server $p
   printf "Running with %02d worker%s on $p, $l-logging, timing out with %ss\n" \
