9aef90a7df43f879317b2f54b1566b6f1477b609
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-10-27 15:09:10 -0300

    fix(restart-memcached): improve pids guessing statement

--- django-restart-memcached.fish
+++ django-restart-memcached.fish
@@ -1,5 +1,5 @@
 function django-restart-memcached -d "Restart memcached on port 10007"
-  set pids (ps ax | grep 10007 | grep memcached | grep -v grep | awk '{print $1}')
+  set pids (ps ax | grep 10007 | grep memcached | awk '{print $1}')
   test "$pids" -ne ""
     and echo "Restarting memcached..."
       and kill $pids
